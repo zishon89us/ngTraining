31d6c473ae8491b11dd3b8aa7b739a604ca6ff4c
Zeeshan Hassan <zishon89us@hotmail.com>
2015-03-11 21:49:24 +0500

    gulp-ruby-sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,23 @@
 var gulp = require('gulp');
 var minifiycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var sass = require('gulp-ruby-sass');
 
-
+//css minification
 gulp.task('css', function () {
     return gulp.src('css/main.css')
         .pipe(minifiycss())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('css/min'));
+        .pipe(autoprefixer('last 5 version'))
+        .pipe(gulp.dest('css/min'))
+        .pipe(notify({message:"CSS Minified."}));
+});
+//sass
+gulp.task('css2', function () {
+    return sass('sass/main.sass', {style:'compressed'}) //.pipe(sass({style:'compressed'}))// this style won't work :: http://stackoverflow.com/questions/28140012/gulp-typeerror-arguments-to-path-join-must-be-strings
+        .pipe(autoprefixer('last 5 version'))
+        .pipe(gulp.dest('css'))
+        .pipe(notify({message:"SASS Done"}));
 });
 
 gulp.task('task2', function () {
@@ -19,10 +29,10 @@ gulp.task('task2', function () {
 });
 
 gulp.task('default', function () {
-    gulp.run('css');
+    gulp.run('css2');
     
-    gulp.watch('css/*.css', function () {
-       gulp.run('css');
+    gulp.watch('sass/*.sass', function () {
+       gulp.run('css2');
     });
 
 });

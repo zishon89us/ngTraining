e0d745592bd790db49bfd9bb923e5028d4cbbc56
Zeeshan Hassan <zishon89us@hotmail.com>
2015-03-11 18:16:40 +0500

    gulp basic

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Zeeshan on 3/11/2015.
+ */
+
+var gulp = require('gulp');
+var minifiycss = require('gulp-minify-css');
+
+
+gulp.task('task1',function(){console.log("task1");});
+gulp.task('task2',function(){console.log("task2");});
+
+gulp.task('default',function(){
+    console.log("YEs");
+
+});
